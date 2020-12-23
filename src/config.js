5114018344aba466fc713efc990c34922bc272d9
TheBestestGuestest <ryanniu314@gmail.com>
2020-12-22 19:02:41 -0800

    initial spotify project api

---
+++ src/config.js
@@ -0,0 +1,9 @@
+export const authEndpoint = 'https://accounts.spotify.com/authorize';
+
+// Replace with your app's client ID, redirect URI and desired scopes
+export const clientId = "2d6518050da043db89266dc384c2a3c2";
+export const redirectUri = "http://localhost:3000/spotify-tier-list-maker#";
+export const scopes = [
+  "user-read-currently-playing",
+  "user-read-playback-state",
+];
