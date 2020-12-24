ff2b0a5ae8704635a59ef29fe1fa73bbc05c9113
TheBestestGuestest <ryanniu314@gmail.com>
2020-12-24 01:52:50 -0800

    halfway through the tutorial

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,10 +4,20 @@ import { Button } from 'react-bootstrap';
 import Header from './Header';
 
 const Home = (props) => {
+  const {
+    REACT_APP_CLIENT_ID,
+    REACT_APP_AUTHORIZE_URL,
+    REACT_APP_REDIRECT_URL
+  } = process.env;
+  
+  const handleLogin = () => {
+    window.location = `${REACT_APP_AUTHORIZE_URL}?client_id=${REACT_APP_CLIENT_ID}&redirect_uri=${REACT_APP_REDIRECT_URL}&response_type=token&show_dialog=true`;
+  };
+
   return (
     <div className="login">
       <Header />
-      <Button variant="info" type="submit">
+      <Button variant="info" type="submit" onClick={handleLogin}>
         Login to spotify
       </Button>
     </div>
