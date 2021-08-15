87a229201e1d54fa778f194ec3ebbd18eeb3a310
TheBestestGuestest <ryanniu314@gmail.com>
2021-08-15 03:05:06 -0700

    optimized search speed a bit

--- src/aws-lambda/index.js
+++ src/aws-lambda/index.js
@@ -1,22 +1,25 @@
 /* This code is used in my AWS Lambda Proxy, and has no effect on the repo. */
+"use strict";
 const axios = require("axios");
 const qs = require("qs");
 const ssm = new(require('aws-sdk/clients/ssm'))();
 
+const clientId = process.env.SPOTIFY_CLIENT_ID;
+const clientSecret = process.env.SPOTIFY_CLIENT_SECRET;
+let accessToken = undefined, expiryTime = undefined;
+
 const getAccessToken = async () => {
     const path = "/spotify-tier-list-maker/";
-    const currentTime = new Date().getTime();
-    const expiryTime = await ssm.getParameter({
-        Name: path.concat("EXPIRY_TIME")
-    }).promise();
-    
-    if (Number(expiryTime.Parameter.Value) < currentTime) {
-        const clientInfo = await ssm.getParameters({
-            Names: ["CLIENT_ID", "CLIENT_SECRET"].map(key => path.concat(key))
+    if (!accessToken || !expiryTime) {
+        const params = await ssm.getParametersByPath({
+            Path: "/spotify-tier-list-maker/"
         }).promise();
-        const clientId = clientInfo.Parameters[0].Value;
-        const clientSecret = clientInfo.Parameters[1].Value;
-        
+        accessToken = params.Parameters[0].Value;
+        expiryTime = Number(params.Parameters[1].Value);
+    }
+    const currentTime = (new Date()).getTime();
+    
+    if (expiryTime < currentTime) {
         const headers = {
             headers: {
                 Accept: 'application/json',
@@ -37,27 +40,24 @@ const getAccessToken = async () => {
                   qs.stringify(data),
                   headers
             );
+            accessToken = result.data["access_token"];
+            expiryTime = currentTime + result.data["expires_in"] * 1000;
             await ssm.putParameter({
                 Name: path.concat("ACCESS_TOKEN"),
-                Value: result.data["access_token"],
+                Value: accessToken,
                 Overwrite: true
             }).promise();
             await ssm.putParameter({
                 Name: path.concat("EXPIRY_TIME"),
-                Value: (currentTime + result.data["expires_in"] * 1000).toString(),
+                Value: expiryTime.toString(),
                 Overwrite: true
             }).promise();
         } catch (error) { console.log(error); }
     }
-    
-    const accessToken = await ssm.getParameter({
-        Name: path.concat("ACCESS_TOKEN")
-    }).promise();
-    return accessToken.Parameter.Value;
 };
 
 const getSearchResult = async (url) => {
-    const accessToken = await getAccessToken();
+    await getAccessToken();
     if (accessToken) {
         try {
             axios.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`;
@@ -74,7 +74,7 @@ exports.handler = async (event) => {
         result = await getSearchResult(event.queryStringParameters.searchURL);
     }
     
-    const allowedOrigin = event.headers.origin === "http://localhost:3000" ? "http://localhost:3000" : "https://ryansniu.github.io";
+    const allowedOrigin = event.headers.origin === "http://localhost:3000" ? event.headers.origin : "https://ryansniu.github.io";
     const response = {
         statusCode: 200,
         headers: {
